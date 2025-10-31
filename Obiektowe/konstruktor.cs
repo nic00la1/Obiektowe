c22165dbe91ff94d2b0fef0fba6c92081790b120
Nicola Kaleta <nic00la.kaleta@gmail.com>
2025-10-31 10:26:27 +0100

    konstruktor

---
+++ Obiektowe/konstruktor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obiektowe
+{
+    public class User
+    {
+        public string Nazwa = "";
+
+        // Konstruktor domyślny
+        public User()
+        {
+            Nazwa = "Bez nazwy";
+            Console.WriteLine("Utworzono użytkownika bez nazwy");
+        }
+
+        // Konstruktor z parametrami
+        public User(string nazwa)
+        {
+            Nazwa = nazwa;
+            Console.WriteLine("Utworzono użytkownika o nazwie: " + Nazwa);
+        }
+
+        // Dekonstruktor - zamyka połączenie z bazą danych / upuszcza strumień z plikiem
+        // Specjalne życzenie - wywalenia kodu naszej klasy
+        ~User()
+        {
+            Console.WriteLine("Usunięto użytkownika");
+        }
+    }
+}
