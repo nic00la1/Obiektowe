692a13df14ca41b187743ceecd5c63f68acbac6b
Nicola Kaleta <nic00la.kaleta@gmail.com>
2025-10-31 10:18:09 +0100

    abstrakcja interfejsy

---
+++ Obiektowe/abstrakcja_interfejsy.cs
@@ -0,0 +1,44 @@
+﻿using static Obiektowe.Kwadrat;
+
+namespace Obiektowe
+{
+   public abstract class Figura
+    {
+        public abstract void Pole();
+    }
+
+    public class Kwadrat : Figura
+    {
+        double Bok;
+        public Kwadrat(double bok)
+        {
+            Bok = Bok;
+        }
+
+
+        public override void Pole()
+        {
+            Console.WriteLine("Pole kwadratu wynosi: " + (Bok * Bok));
+        }
+
+        public interface IRysowalny
+        {
+            void Rysuj();
+        }
+    }
+
+    public class Kolo : IRysowalny
+    {
+        public double Promien;
+
+        public Kolo(double promien)
+        {
+            Promien = promien;
+        }
+
+        public void Rysuj()
+        {
+            Console.WriteLine("Narysowano : D");
+        }
+    }
+}
